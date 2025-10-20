d1d7b419a9234b7870566a59faccc8fac11d8bf1
Md Khokanuzzaman khokan <khokan@jotno.net>
2025-10-20 15:36:51 +0600

    Clean up package structure for publishing

--- test/pdf_render_test.dart
+++ test/pdf_render_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pdf_render_maintained/pdf_render.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('pdf_render');
